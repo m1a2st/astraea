341670412f1f95d311605e18da1b2ddcbff766f8
m1a2st <s7133700@gmail.com>
2024-11-18 20:44:48 +0800

    wip

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -165,7 +165,15 @@ public class SendYourData {
           };
       producer =
           new KafkaProducer<>(
-              Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers),
+              Map.of(
+                  ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                  bootstrapServers,
+                  ProducerConfig.COMPRESSION_TYPE_CONFIG,
+                  "gzip",
+                  ProducerConfig.LINGER_MS_CONFIG,
+                  "1000",
+                  ProducerConfig.BATCH_SIZE_CONFIG,
+                  "8192"),
               serializer,
               new ByteArraySerializer());
     }
