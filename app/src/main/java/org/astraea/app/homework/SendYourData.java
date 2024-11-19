b44e6802544e09989693217ab248f9de83bada44
m1a2st <s7133700@gmail.com>
2024-11-19 23:23:38 +0800

    wip

--- app/src/main/java/org/astraea/app/homework/SendYourData.java
+++ app/src/main/java/org/astraea/app/homework/SendYourData.java
@@ -168,8 +168,10 @@ public class SendYourData {
                   bootstrapServers,
                   ProducerConfig.COMPRESSION_TYPE_CONFIG,
                   "zstd",
+                  ProducerConfig.LINGER_MS_CONFIG,
+                  "5000",
                   ProducerConfig.BATCH_SIZE_CONFIG,
-                  "4096"),
+                  "8192"),
               serializer,
               new ByteArraySerializer());
     }
